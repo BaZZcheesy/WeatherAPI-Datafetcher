2eebfd012e5914b093c504e18bb7c947f6e40a74
Nathaniel Walser <nathaniel.walser@esp-engineering.com>
2023-12-20 10:51:00 +0100

    restructured project

---
+++ M347_Data_Fetcher/Data/Location.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace M347_Data_Fetcher.Data
+{
+    public class Location
+    {
+        [JsonProperty("name")]
+        public required string Name { get; set; }
+
+        [JsonProperty("region")]
+        public required string Region { get; set; }
+
+        [JsonProperty("country")]
+        public required string Country { get; set; }
+
+        [JsonProperty("lat")]
+        public required decimal Lat { get; set; }
+
+        [JsonProperty("lon")]
+        public required decimal Lon { get; set; }
+
+        [JsonProperty("localtime_epoch")]
+        public required int LocaltimeEpoch { get; set; }
+
+        [JsonProperty("localtime")]
+        public required string Localtime { get; set; }
+    }
+}
---
+++ M347_Data_Fetcher/Data/Response.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Serialization;
+
+namespace M347_Data_Fetcher.Data
+{
+    public class Response
+    {
+        public int Id { get; set; }
+        public string dateInserted { get; set; }
+
+        required public Location Location { get; set; }
+
+        required public Current Current { get; set; }
+
+        //JsonProperty("")
+        //required public int ResponseCode { get; set; };
+    }
+}
